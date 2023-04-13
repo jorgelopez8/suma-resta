3a9c462b003656ea4e9ff538911bb0df9cd31d23
jorgelopez8 <123724150+jorgelopez8@users.noreply.github.com>
2023-04-13 11:08:26 +0200

    let

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 alert("A continuacion se le pediran unos numeros");
-let nombre = prompt("Introduzca aquí el primer número:");
+let num1 = parseFloat(prompt("Introduzca aquí el primer número:"));
+let num2 = parseFloat(prompt("Ahora introduzca el siguiente número"))
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicación = num1 * num2;
+let división = num2 !== 0 ? num1 / num2 : "No se puede dividir entre cero";
+
+alert("El resultado de la suma es: " + suma);
+alert("El resultado de la resta es: " + resta);
+alert("El resultado de la multiplicación es: " + multiplicacion);
+alert("El resultado de la división es: " + division);
